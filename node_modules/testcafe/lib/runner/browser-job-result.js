c61d155eb47c63b0adaf8914046bc1c9f7d2e5a7
Stephany Henrique <stephany.batista@outlook.com>
2018-11-15 08:50:07 -0300

    Excluindo node_modules do repositório

--- node_modules/testcafe/lib/runner/browser-job-result.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-exports.__esModule = true;
-exports.default = {
-    done: 'browser-job-done',
-    errored: 'browser-job-errored',
-    aborted: 'browser-job-aborted'
-};
-module.exports = exports['default'];
